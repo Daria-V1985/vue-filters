9751f2591ea4c9508e13c69f1ba2f611e06590e9
Daria <darjana29@gmail.com>
2025-05-18 22:49:34 +0300

    init: add project

---
+++ vue-shop-filters/src/components/CardList.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div class="catalog__products products">
+    <div class="products__card">
+      <TheCard 
+      v-for="item in items"
+      :key = "item.id"
+      :category = "item.category"
+      :title = "item.title" 
+      :image-url = "item.imageUrl"
+      :price = "item.price"
+      :color = "item.color"
+      />
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+  import TheCard from '../components/TheCard.vue';
+
+  defineProps ({
+    items: Array
+  })
+</script>
---
+++ vue-shop-filters/src/components/LengthFilter.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div class="filter-catalog__item">
+    <div class="filter-catalog__item-title">dress length</div>
+    <div class="filter-catalog__item-content content-item">
+      <div class="content-item__container">
+        <div class="content-item__checkbox checkbox">
+          <input id="c_15" class="checkbox__input" type="checkbox">
+          <label for="c_15" class="checkbox__label">short</label>
+        </div>
+        <div class="content-item__checkbox checkbox">
+          <input id="c_16" class="checkbox__input" type="checkbox">
+          <label for="c_16" class="checkbox__label">knee length</label>
+        </div>
+        <div class="content-item__checkbox checkbox">
+          <input id="c_17" class="checkbox__input" type="checkbox" checked>
+          <label for="c_17" class="checkbox__label">hight low</label>
+        </div>
+        <div class="content-item__checkbox checkbox">
+          <input id="c_18" class="checkbox__input" type="checkbox">
+          <label for="c_18" class="checkbox__label">long</label>
+        </div>
+        <div class="content-item__checkbox checkbox">
+          <input id="c_19" class="checkbox__input" type="checkbox">
+          <label for="c_19" class="checkbox__label">midi</label>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
